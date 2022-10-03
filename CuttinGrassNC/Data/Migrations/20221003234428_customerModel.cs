c9dc64e2db711852032e077b2250adac947b9637
aric <aricwilliamst@gmail.com>
2022-10-03 19:46:39 -0400

    Add project files.

---
+++ CuttinGrassNC/Data/Migrations/20221003234428_customerModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CuttinGrassNC.Data.Migrations
+{
+    public partial class customerModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    phoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZipCode = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BusinessName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsPayingCustomer = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ CuttinGrassNC/Models/Customers.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuttinGrassNC.Models
+{
+	public class Customers
+	{
+		[Key]
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? phoneNumber { get; set; }
+		public string? Address { get; set; }
+		public string? City{ get; set; }
+		public int ZipCode { get; set; }
+		public string? Email {get; set;}
+		public string? BusinessName { get; set; }
+		public bool IsPayingCustomer { get; set; }	
+
+
+	}
+}
